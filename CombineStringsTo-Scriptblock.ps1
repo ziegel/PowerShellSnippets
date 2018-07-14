5550199115480c77a7186ee8b986c27961313f67
ziegel <sandor.teglasy@gmail.com>
2018-07-14 17:50:08 +0300

    Update CombineStringsTo-Scriptblock.ps1

--- CombineStringsTo-Scriptblock.ps1
+++ CombineStringsTo-Scriptblock.ps1
@@ -1,6 +1,6 @@
 #Combining 2 strings to scriptblock allows it to be run in PowerShell where a parameter of type ScriptBlock (and Not String) is expected
 #Based on https://www.thomasmaurer.ch/2011/11/powershell-convert-string-to-scriptblock/
-function CombineStringTo-Scriptblock ()
+function CombineStringsTo-Scriptblock ()
 {
     [CmdletBinding()]
     param(
